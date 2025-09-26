b63681730213bbd2d8b63aa2578bd6b4d50de202
LuckyWhale13 <luckywhale13@gmail.com>
2025-09-26 17:11:10 +0200

    bug

--- src/script/interpreter.h
+++ src/script/interpreter.h
@@ -183,7 +183,7 @@ private:
     const CTransaction txTo;
 
 public:
-    MutableTransactionSignatureChecker(const CMutableTransaction* txToIn, unsigned int nInIn, const CAmount& amount, ChainSigVersion chainSigVersion = CHAINSIG_VERSION_LATEST) : TransactionSignatureChecker(&txTo, nInIn, amount), txTo(*txToIn, chainSigVersion) {}
+    MutableTransactionSignatureChecker(const CMutableTransaction* txToIn, unsigned int nInIn, const CAmount& amount, ChainSigVersion chainSigVersion = CHAINSIG_VERSION_LATEST) : TransactionSignatureChecker(&txTo, nInIn, amount, chainSigVersion), txTo(*txToIn) {}
 };
 
 bool EvalScript(std::vector<std::vector<unsigned char> >& stack, const CScript& script, unsigned int flags, const BaseSignatureChecker& checker, SigVersion sigversion, ScriptError* error = NULL);
