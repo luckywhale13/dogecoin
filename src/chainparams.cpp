f410ba75edcc75d0df919fe2de20b1bfff0e257d
luckywhale13 <luckywhale13@gmail.com>
2025-10-28 03:28:42 +0100

    cosmetic

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -177,36 +177,36 @@ public:
         fMineBlocksOnDemand = false;
 
         checkpointData = (CCheckpointData) {
-                boost::assign::map_list_of
-                        (     0, uint256S("0x9b7bce58999062b63bfb18586813c42491fa32f4591d8d3043cb4fa9e551541b"))
-                        (     1, uint256S("0xcf2f78756f0fa64bc4ce80e6d500661cc4c20f2be28c7d859467dceb0adfa2de"))
-                        (    53, uint256S("0x9343eae8db94d5d5e945b0c0a6b83647b6a3a8fd89cd170a757e06dcbf2e3bed"))
-                        (   117, uint256S("0x5208d62f44467e800a92bfb18fc0fd4c39d9fed28f4ad160262f96dd90111ec3"))
-                        (   200, uint256S("0xa5ce00c4aab4f9deccbef0af27adadb29cbf111eb442e92895d7302eb047ad4e"))
-                        (  6452, uint256S("0xe502fdfb3a35ee853ccd4a68433b1f9bbe3295c7d453fbcc484d06a766971475"))
-                        ( 10978, uint256S("0x88fcee5009a0febf7832750d0246bf4a9b88f8195befc795e5f34b0d1e0e92f9"))
-                        ( 17954, uint256S("0x7d40a9b80dd1b585b36e092aefcd3e579ef38f3180ea55dac53ead486f5d9cd2"))
-                        ( 23978, uint256S("0x6f111b6eef7dccc2da3c85014964aa402f39c684ba5709b576777503c87141af"))
-                        ( 33212, uint256S("0xe3b53359c1b088ec1f772d53eaa765d5c7410f0d9914e69bdb2a0fc881ddc9e8"))
-                        ( 45527, uint256S("0x41849cf3bd7b819a6a994d17dcfb1cbc7eadfe63fa61cc1411cfe42177abc06a"))
-                        ( 57484, uint256S("0x807fb268c7faabc70cc95c1027cbf1e555834e5bf9e19e01ef785be88853ae88"))
-                        ( 69240, uint256S("0x07d2b42e1898d59594b10f26fdc76d4f970a10b4b330237012f48eb489c8d744"))
-                        ( 73892, uint256S("0x5b43092ef40969b65878cee7c568e622a4a9d950a130858a10914402797f96b1"))
-                        ( 168312, uint256S("0x26816c8861d283ab9bdf4539e5398f65ae5687b90f62cee28036f6e8387933e8"))
-                        ( 170421, uint256S("0x647540c0bce26bdcc4f863a6163c1dc86824899835af31cb9d649a85caca38ec"))
-                        ( 170924, uint256S("0x28e1a097871c66d25021091fbd68d0f0301d3fc0b106e8d7c6e190e39a20b335"))
-                        ( 172330, uint256S("0x8458c3eeda44dc11352edad04e0eb69ae898c69c0dded3b3903b37f5bf352555"))
-                        ( 320000, uint256S("0x3da52a29c4a7528d7624a4a74d659c08fbc7808cd4c217d152ba5981fc318b68"))
-                        ( 640000, uint256S("0x99e1d5c9283b9b70c2ff3dec80aa1b9517822c789b4dcbf2b984c395e99d487d"))
+            boost::assign::map_list_of
+            (     0, uint256S("0x9b7bce58999062b63bfb18586813c42491fa32f4591d8d3043cb4fa9e551541b"))
+            (     1, uint256S("0xcf2f78756f0fa64bc4ce80e6d500661cc4c20f2be28c7d859467dceb0adfa2de"))
+            (    53, uint256S("0x9343eae8db94d5d5e945b0c0a6b83647b6a3a8fd89cd170a757e06dcbf2e3bed"))
+            (   117, uint256S("0x5208d62f44467e800a92bfb18fc0fd4c39d9fed28f4ad160262f96dd90111ec3"))
+            (   200, uint256S("0xa5ce00c4aab4f9deccbef0af27adadb29cbf111eb442e92895d7302eb047ad4e"))
+            (  6452, uint256S("0xe502fdfb3a35ee853ccd4a68433b1f9bbe3295c7d453fbcc484d06a766971475"))
+            ( 10978, uint256S("0x88fcee5009a0febf7832750d0246bf4a9b88f8195befc795e5f34b0d1e0e92f9"))
+            ( 17954, uint256S("0x7d40a9b80dd1b585b36e092aefcd3e579ef38f3180ea55dac53ead486f5d9cd2"))
+            ( 23978, uint256S("0x6f111b6eef7dccc2da3c85014964aa402f39c684ba5709b576777503c87141af"))
+            ( 33212, uint256S("0xe3b53359c1b088ec1f772d53eaa765d5c7410f0d9914e69bdb2a0fc881ddc9e8"))
+            ( 45527, uint256S("0x41849cf3bd7b819a6a994d17dcfb1cbc7eadfe63fa61cc1411cfe42177abc06a"))
+            ( 57484, uint256S("0x807fb268c7faabc70cc95c1027cbf1e555834e5bf9e19e01ef785be88853ae88"))
+            ( 69240, uint256S("0x07d2b42e1898d59594b10f26fdc76d4f970a10b4b330237012f48eb489c8d744"))
+            ( 73892, uint256S("0x5b43092ef40969b65878cee7c568e622a4a9d950a130858a10914402797f96b1"))
+            ( 168312, uint256S("0x26816c8861d283ab9bdf4539e5398f65ae5687b90f62cee28036f6e8387933e8"))
+            ( 170421, uint256S("0x647540c0bce26bdcc4f863a6163c1dc86824899835af31cb9d649a85caca38ec"))
+            ( 170924, uint256S("0x28e1a097871c66d25021091fbd68d0f0301d3fc0b106e8d7c6e190e39a20b335"))
+            ( 172330, uint256S("0x8458c3eeda44dc11352edad04e0eb69ae898c69c0dded3b3903b37f5bf352555"))
+            ( 320000, uint256S("0x3da52a29c4a7528d7624a4a74d659c08fbc7808cd4c217d152ba5981fc318b68"))
+            ( 640000, uint256S("0x99e1d5c9283b9b70c2ff3dec80aa1b9517822c789b4dcbf2b984c395e99d487d"))
         };
 
         chainTxData = ChainTxData{
-                // Data as of block ed7d266dcbd8bb8af80f9ccb8deb3e18f9cc3f6972912680feeb37b090f8cee0 (height 4303965).
-                // Tx estimate based on average between 2021-07-01 (3793538) and 2022-07-01 (4288126)
-                1760588747, // * UNIX timestamp of last checkpoint block
-                1018846,    // * total number of transactions between genesis and last checkpoint
-                            //   (the tx=... number in the SetBestChain debug.log lines)
-                0.023       // * estimated number of transactions per second after checkpoint
+            // Data as of block ed7d266dcbd8bb8af80f9ccb8deb3e18f9cc3f6972912680feeb37b090f8cee0 (height 4303965).
+            // Tx estimate based on average between 2021-07-01 (3793538) and 2022-07-01 (4288126)
+            1760588747, // * UNIX timestamp of last checkpoint block
+            1018846,    // * total number of transactions between genesis and last checkpoint
+                        //   (the tx=... number in the SetBestChain debug.log lines)
+            0.023       // * estimated number of transactions per second after checkpoint
         };
     }
 };
@@ -330,17 +330,15 @@ public:
         fMineBlocksOnDemand = false;
 
         checkpointData = (CCheckpointData) {
-                boost::assign::map_list_of
-                        (      0, uint256S("0x324635c8e36f663b0adb126a21ad0bd7fa43cc5c5f15aec992bf4dde650bc0ea"))
+            boost::assign::map_list_of
+            (      0, uint256S("0x324635c8e36f663b0adb126a21ad0bd7fa43cc5c5f15aec992bf4dde650bc0ea"))
         };
 
         chainTxData = ChainTxData{
-                // Data as of block 0x324635c8e36f663b0adb126a21ad0bd7fa43cc5c5f15aec992bf4dde650bc0ea (height 0).
-                // Tx estimate based on average between 2021-07-01 (3793538) and 2022-07-01 (4288126)
-                0, // * UNIX timestamp of last checkpoint block
-                0,   // * total number of transactions between genesis and last checkpoint
-                //   (the tx=... number in the SetBestChain debug.log lines)
-                0        // * estimated number of transactions per second after checkpoint
+            // Data as of block 0x324635c8e36f663b0adb126a21ad0bd7fa43cc5c5f15aec992bf4dde650bc0ea (height 0).
+            0,  // * UNIX timestamp of last checkpoint block
+            0,  // * total number of transactions between genesis and last checkpoint
+            0   // * estimated number of transactions per second after checkpoint
         };
 
     }
